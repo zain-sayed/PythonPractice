0e8f16e68948814c9cbbe9e200b5dfd02d52a110
Zain <zain.sayed6@gmail.com>
2019-09-24 23:32:56 -0400

    First CSV file output

--- Final Project/WebScraper.py
+++ Final Project/WebScraper.py
@@ -7,11 +7,14 @@ import bs4
 import requests
 import selenium
 from selenium import webdriver
+import csv
 
 dateList = []
 highList =[]
 lowList = []
 marketCap = []
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\zains\Desktop\chromedriver.exe')
 
 r = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end=20190925')
 
@@ -24,4 +27,12 @@ for item in tr:
     lowList.append(item.find_all('td')[3].text)
     marketCap.append(item.find_all('td')[6].text)
 
-print(len(dateList))
+row0 = ['Date','High','Market Cap']
+rows = zip(dateList,highList,marketCap)
+
+with open('coinMarketExample.csv','w',encoding='utf-8',newline='')as csvfile:
+    links_writer = csv.writer(csvfile)
+    links_writer.writerow(row0)
+    for item in rows:
+        links_writer.writerow(item)
+
